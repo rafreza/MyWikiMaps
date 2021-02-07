9f7dc6af2348f53ce3dab99fe3d00bcbc5764ebd
vagrant <galit.gerasimov3@gmail.com>
2021-02-07 16:44:28 +0000

    Updated maps

--- leaflet/map.js
+++ leaflet/map.js
@@ -1,4 +1,28 @@
-const map = L.map('map', {
-  center: [51.505, -0.09],
-  zoom: 13
-});
+const map = L.mapbox.map('map')
+.setView([40, -74.50], 9)
+.addLayer(L.mapbox.styleLayer('mapbox://styles/antotm99/ckkufbk2v2rkf17pewagzbfdo'));
+
+
+
+
+      // const map = L.map('map',{
+      //   center: [43.64701, -79.39425],
+      //   zoom: 15
+      //   });
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        const map2 = L.map('map2',{
+        center: [43.64701, -79.39425],
+        zoom: 15
+        });
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map2);
+
+
+
+
