a3963289f1d478a6e368b9e1980c1a3fb165bbc1
vagrant <galit.gerasimov3@gmail.com>
2021-02-09 02:16:36 +0000

    login fixed

--- routes/maps.js
+++ routes/maps.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get("/maps", (req, res) => {
-  res.render("create");
 
-});
+router.get("/maps", (req, res) => {
+  const templateVars = { user_id: req.session['user_id'], email: req.session['email']}
+  res.render("create", templateVars);
+})
 
 module.exports = router;
 
