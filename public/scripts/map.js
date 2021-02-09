b9fdca6ab89cfd904a36a02e9c249dc4e786ea3a
Rafin Reza <rafintokyo@gmail.com>
2021-02-09 02:35:29 +0000

    merge master

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -18,15 +18,7 @@ const map = L.map('map',{
   L.tileLayer('https://api.mapbox.com/styles/v1/antotm99/ckkufbk2v2rkf17pewagzbfdo/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW50b3RtOTkiLCJhIjoiY2trdWVueTNuMHJiNzJucnVvcmRvczBoNiJ9.xCn9pVxKwusP1dHXrOJhNA', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-  /*
-  let marker = L.marker([43.6555, -79.4138]).addTo(map);
-  let marker2 = L.marker([43.6568, -79.4069]).addTo(map);
-  let marker3 = L.marker([43.6550, -79.4144]).addTo(map);
 
-  let popup = marker.bindPopup('Cafe Diplomatico');
-  let popup2 = marker2.bindPopup('The Little Jerry');
-  let popup3 = marker3.bindPopup('The Walton');
-  */
 
   map.on('click', addMarkerInfo);
 
--- routes/maps.js
+++ routes/maps.js
@@ -2,15 +2,10 @@ const express = require('express');
 const router  = express.Router();
 
 
-<<<<<<< HEAD
-
-});
-=======
 router.get("/maps", (req, res) => {
   const templateVars = { user_id: req.session['user_id'], email: req.session['email']}
   res.render("create", templateVars);
 })
->>>>>>> master
 
 module.exports = router;
 
