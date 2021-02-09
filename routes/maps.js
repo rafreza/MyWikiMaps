a2a2608a75de67553a96fda290b9f480856bfa0c
Rafin Reza <rafintokyo@gmail.com>
2021-02-09 21:45:51 +0000

    add profile routing, add map form

--- routes/maps.js
+++ routes/maps.js
@@ -7,23 +7,6 @@ router.get("/maps", (req, res) => {
 
   const templateVars = { user_id: req.session['user_id'], email: req.session['email']};
   res.render('create', templateVars);
-  const getMaps = () => {
-    let queryString = `
-    SELECT * FROM maps
-    `;
-    return pool.query(queryString).then(res => res.rows[0]);
-  };
-
-  getMaps()
-    .then(maps => {
-      res.send({ maps }).catch(err => {
-        res.status(500).json({ error: err.message });
-      });
-    })
-    .catch(e => {
-      res.send(e);
-
-    });
   });
 
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const { pool } = require('../dbConfig');
 const router  = express.Router();
 
 router.get("/profile", (req, res) => {
   const templateVars = { user_id: req.session['user_id'], email: req.session['email']}
   res.render("profile", templateVars);
+
+  const getMaps = function(limit) {
+
+    let queryString = `
+        SELECT * FROM maps
+        `;
+
+    return pool.query(queryString, queryParams).then(res => res.rows);
+  };
+
+  getAllMaps()
+  .then(maps => {
+    res.send({ maps }).catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+  })
+  .catch(e => {
+    console.error(e);
+    res.send(e);
+  });
 
 });
 
