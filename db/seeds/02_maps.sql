a2305fe221529ce688307affda037e298d331763
Anto Ter-Mesropian <antotm99@gmail.com>
2021-02-10 00:44:56 +0000

    added seeds for maps and points

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,3 +1,4 @@
 INSERT INTO maps (user_id, title, description, image_url)
 VALUES
-(1, 'Best Little Italy Spots', 'These are the best spots to check out in the area', 'https://cache-graphicslib.viator.com/graphicslib/thumbs674x446/37232/SITours/dinner-with-chef-little-italy-vip-food-tour-in-toronto-395768.jpg');
+(1, 'Best Little Italy Spots', 'These are the best spots to check out in the area', 'https://cache-graphicslib.viator.com/graphicslib/thumbs674x446/37232/SITours/dinner-with-chef-little-italy-vip-food-tour-in-toronto-395768.jpg'),
+(1, 'Best Poutines in Montreal', 'These are the best spots to get yourself some delicious Poutine!', 'https://welcomesignproject.files.wordpress.com/2010/10/img_1994.jpg');
